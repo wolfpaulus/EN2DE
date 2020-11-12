d1e93903127d46bbe3f899554b5db99a8e1a7992
Wolf Paulus <wolf@paulus.com>
2020-11-12 15:40:19 -0700

    set logger, update cleanup doc string

--- lambda/app.py
+++ lambda/app.py
@@ -2,16 +2,14 @@ import logging
 
 from boto3 import client
 
+logger = logging.getLogger(__name__)
 logging.getLogger().setLevel(logging.INFO)
 
 
 def lambda_handler(event: dict, context) -> dict:
     """
     :param event: input data, usually a dict, but can also be list, str, int, float, or NoneType type.
-    :param context: object w/ methods a. properties providing info about invocation, function, and execution environment
-        https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
-    Boto3 Translate client documentation
-        https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/translate.html
+    :param context: object providing info about invocation, function, and execution environment
     :return: dict with TranslatedText, SourceLanguageCode, TargetLanguageCode
     """
     logging.info(str(event))
